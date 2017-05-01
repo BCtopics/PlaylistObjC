348dd359618da89b1f8c3a746b16debe4c4ce54c
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-01 10:08:01 -0600

    Project Cleanup

---
+++ PlaylistObjC/PlaylistObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PlaylistObjC
+//
+//  Created by Bradley GIlmore on 5/1/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlaylistObjC/PlaylistObjC/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  PlaylistObjC
+//
+//  Created by Bradley GIlmore on 5/1/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+@end
